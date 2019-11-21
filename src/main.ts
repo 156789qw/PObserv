20e6a4c193a2f43d22eaf420cd99a0c1d1be19f9
DenisGromenkoV <156789qw@gmail.com>
2019-11-21 22:03:32 +0200

    first commit2

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-console.log("dsfgds");
+interface IObserverEventListener {
+  update(providerEventManager: IProviderEventManager): void;
+}
+
+interface IProviderEventManager {
+  attach(observerEventListener: IObserverEventListener): void;
+  detach(observerEventListener: IObserverEventListener): void;
+  notify(): void;
+}
+class ProviderEventManager implements IProviderEventManager {
+  private observerEventListeners: IObserverEventListener[] = [];
+
+  attach(observerEventListener: IObserverEventListener): void {
+    this.observerEventListeners.push(observerEventListener);
+  }
+  detach(observerEventListener: IObserverEventListener): void {
+    var index = this.observerEventListeners.indexOf(observerEventListener);
+    if (index !== -1) {
+      this.observerEventListeners.splice(index, 1);
+    }
+  }
+  notify(): void {
+    for (const observer of this.observerEventListeners) {
+      observer.update(this);
+    }
+  }
+}
